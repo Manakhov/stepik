a80e92fd7de9bf290f65c4753320e65ce054210e
Svyatoslav <manakhovsvyatoslav@gmail.com>
2020-08-17 18:24:35 +0300

    refresh task_1.2

--- task_1.2.py
+++ task_1.2.py
@@ -1,16 +1,15 @@
-def comparison(list_obj, obj_comp):
-    for obj in list_obj:
-        if obj_comp is obj:
-            return True
-    return False
-
-
 objects = [1, 2, 1, 2, 3]
 list_obj = []
 ans = 1
 for obj in objects:
-    if comparison(list_obj, obj):
-        ans += 1
+    i = False
+    for obj_comp in list_obj:
+        if obj is obj_comp:
+            ans += 1
+            i = True
+            break
+    if i:
+        continue
     else:
         list_obj.append(obj)
 print(ans)
