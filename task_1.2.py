27435980054c7467d4c3a990b19f4848e0c0649f
Svyatoslav <manakhovsvyatoslav@gmail.com>
2020-08-17 18:28:15 +0300

    ready task_1.2

--- task_1.2.py
+++ task_1.2.py
@@ -1,15 +1,8 @@
 objects = [1, 2, 1, 2, 3]
 list_obj = []
-ans = 1
+ans = 0
 for obj in objects:
-    i = False
-    for obj_comp in list_obj:
-        if obj is obj_comp:
-            ans += 1
-            i = True
-            break
-    if i:
-        continue
-    else:
+    if obj not in list_obj:
         list_obj.append(obj)
+        ans += 1
 print(ans)
